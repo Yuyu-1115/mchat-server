7455ed6f1cdc9ed35e3e06e05cdcc872fe0f7d9f
Yuyu-1115 <105599462+Yuyu-1115@users.noreply.github.com>
2026-05-09 15:16:44 +0000

    fix: optimize message_queue_thread

--- src/server/message_queue.c
+++ src/server/message_queue.c
@@ -53,10 +53,8 @@ message_packet_t message_queue_pop() {
 
 void *message_consumer_thread(__attribute__((unused)) void *arg) {
   while (1) {
-    while (mq.size > 0) {
-      message_packet_t packet = message_queue_pop();
-      broadcast(&packet);
-    }
+    message_packet_t packet = message_queue_pop();
+    broadcast(&packet);
   }
   return NULL;
 }
